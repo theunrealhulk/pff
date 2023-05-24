11464e18bd43462089394109d692aeaa0fe536ab
theunrealhulk <chafiaay.abj@gmail.com>
2023-05-24 02:19:29 +0100

    add logo

--- UI/assets/script.js
+++ UI/assets/script.js
@@ -1,152 +1,148 @@
 function  deleteProject(id){
 
-    deleteFromDb(id,"Id","/deleteProject")
+  deleteFromDb(id,"Id","/deleteProject")
 }
 function requestOptions(data)
 {
-    return {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(data)
-      };
+  return {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(data)
+    };
 }
 function readFromDb(url,data,element,renderHTMLfunction){
-    
-      fetch(url,requestOptions(data)).then(result)
-      .then(response => response.json())
-      .then((data) =>{
-         console.log(data)
-             if (data.success) {
-                element.innerHTML=renderHTMLfunction(data)
-             }
-             else{
-                 Swal.fire({
-                     icon: 'error',
-                     title: 'Oops...',
-                     text: 'Something went wrong!',
-                   })
-             }
-      })
+  
+    fetch(url,requestOptions(data)).then(result)
+    .then(response => response.json())
+    .then((data) =>{
+       console.log(data)
+           if (data.success) {
+              element.innerHTML=renderHTMLfunction(data)
+           }
+           else{
+               Swal.fire({
+                   icon: 'error',
+                   title: 'Oops...',
+                   text: 'Something went wrong!',
+                 })
+           }
+    })
 }
 
 function changeStatus(statusId,assignmentId)
 {
-    fetch ("/updateStatus",requestOptions({statusId:statusId,assignmentId:assignmentId}))
-    .then(response => response.json())
-    .then((data) =>{
-        if(data.success){
-            window.location.reload()
-        }
-    })
+  fetch ("/updateStatus",requestOptions({statusId:statusId,assignmentId:assignmentId}))
+  .then(response => response.json())
+  .then((data) =>{
+      if(data.success){
+          window.location.reload()
+      }
+  })
 }
 function  deleteAssignment(id){
 
-    deleteFromDb(id,"Id","/deleteAssignment")
+  deleteFromDb(id,"Id","/deleteAssignment")
 }
 function deleteFromDb(val,columnName,url)
 {
-    console.log("deleteFromDb")
-    let data={}
-    data[columnName]=val
-    Swal.fire({
-        title: 'Are you sure?',
-        // text: "You won't be able to revert this!",
-        icon: 'warning',
-        showCancelButton: true,
-        confirmButtonColor: '#3085d6',
-        cancelButtonColor: '#d33',
-        confirmButtonText: 'Yes, delete it!'
-      }).then((result) => {
-        if (result.isConfirmed) {
-              console.log(requestOptions(data));
-             fetch(url,requestOptions(data)).then(result)
-             .then(response => response.json())
-             .then((data) =>{
-                console.log(data)
-                    if (data.success) {
-                        window.location.reload()
-                    }
-                    else{
-                        Swal.fire({
-                            icon: 'error',
-                            title: 'Oops...',
-                            text: 'Something went wrong!',
-                          })
-                    }
-             })
-        }
-      })
+  console.log("deleteFromDb")
+  let data={}
+  data[columnName]=val
+  Swal.fire({
+      title: 'Are you sure?',
+      // text: "You won't be able to revert this!",
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonColor: '#3085d6',
+      cancelButtonColor: '#d33',
+      confirmButtonText: 'Yes, delete it!'
+    }).then((result) => {
+      if (result.isConfirmed) {
+            console.log(requestOptions(data));
+           fetch(url,requestOptions(data)).then(result)
+           .then(response => response.json())
+           .then((data) =>{
+              console.log(data)
+                  if (data.success) {
+                      window.location.reload()
+                  }
+                  else{
+                      Swal.fire({
+                          icon: 'error',
+                          title: 'Oops...',
+                          text: 'Something went wrong!',
+                        })
+                  }
+           })
+      }
+    })
 }
 $(document).ready(function(){
-    var projectId=""
-    
-    $('.modal').modal();
-    $('.collapsible').collapsible();
-    $('select').formSelect();
-    $('.dropdown-trigger').dropdown();
-    $('.T').click((e)=>{
-        projectId=e.target.getAttribute('data-project-id');
-    })
+  var projectId=""
+  
+  $('.modal').modal();
+  $('.collapsible').collapsible();
+  $('select').formSelect();
+  $('.dropdown-trigger').dropdown();
+  $('.T').click((e)=>{
+      projectId=e.target.getAttribute('data-project-id');
+  })
 
-    $('form').submit(function(e) {
-        e.preventDefault();
+  $('form').submit(function(e) {
+      e.preventDefault();
 
-        // Get the submitted form
-        var submittedForm = $(this);
-        // Get all form values
-        var formValues = {};
-        submittedForm.find('input, select, textarea').each(function() {
-            var inputName = $(this).attr('name')==undefined?'noName': $(this).attr('name');
-            var inputValue = $(this).val();
-            formValues[inputName] = inputValue;
-            formValues["ProjectId"] = projectId;
+      // Get the submitted form
+      var submittedForm = $(this);
+      // Get all form values
+      var formValues = {};
+      submittedForm.find('input, select, textarea').each(function() {
+          var inputName = $(this).attr('name')==undefined?'noName': $(this).attr('name');
+          var inputValue = $(this).val();
+          formValues[inputName] = inputValue;
+          formValues["ProjectId"] = projectId;
 
-        });
+      });
 
-        // Log form values to the console
-        console.log(formValues);
-        console.log(submittedForm[0].name);
-        const requestOptions = {
-            method: 'POST',
-            headers: {
-              'Content-Type': 'application/json'
-            },
-            body: JSON.stringify(formValues)
-          };
-          console.log(requestOptions);
-        fetch(submittedForm[0].action, requestOptions)
-        .then(response => response.json())
-        .then(data => {
-            console.log('Response:', data);
-            // Handle the response data
-            let successMessage = submittedForm[0].name=="login"?"successfully logged In":"Your created a new User"
-            
-            Swal.fire({
-                icon: 'success',
-                title: successMessage,
-                showConfirmButton: false,
-                timer: 1500
-              }).then(()=>{
-                  window.location.reload();
-              })
-            
-        })
-        .catch(error => {
-            let failMessage = submittedForm[0].name=="login"?"Fail to log In":"Fail to run Operation"
-            console.error('Error:', error);
-            // Handle any errors that occurred during the request
-            Swal.fire({
-                icon: 'error',
-                title: failMessage,
-                text: error,
-              })
-        });
-    });
-    
-    
-
-    
+      // Log form values to the console
+      console.log(formValues);
+      console.log(submittedForm[0].name);
+      const requestOptions = {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json'
+          },
+          body: JSON.stringify(formValues)
+        };
+        console.log(requestOptions);
+      fetch(submittedForm[0].action, requestOptions)
+      .then(response => response.json())
+      .then(data => {
+          console.log('Response:', data);
+          // Handle the response data
+          let successMessage = submittedForm[0].name=="login"?"successfully logged In":"Your created a new User"
+          
+          Swal.fire({
+              icon: 'success',
+              title: successMessage,
+              showConfirmButton: false,
+              timer: 1500
+            }).then(()=>{
+                window.location.reload();
+            })
+          
+      })
+      .catch(error => {
+          let failMessage = submittedForm[0].name=="login"?"Fail to log In":"Fail to run Operation"
+          console.error('Error:', error);
+          // Handle any errors that occurred during the request
+          Swal.fire({
+              icon: 'error',
+              title: failMessage,
+              text: error,
+            })
+      });
+  }); 
 })
 
